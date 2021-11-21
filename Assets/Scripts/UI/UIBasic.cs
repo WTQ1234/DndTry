c281c80d75a30cadfce8156dc7ce987fa727eea3
“WTQ1234” <815532046@qq.com>
2021-11-21 18:58:39 +0800

    尝试添加多语言处理

--- Assets/Scripts/UI/UIBasic.cs
+++ Assets/Scripts/UI/UIBasic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using FairyGUI;
+using Polyglot;
 
 public class UIParamBasic
 {
@@ -12,10 +13,17 @@ public class UIParamBasic
     }
 }
 
+public class TextParam
+{
+    public string defaultValue;
+    public List<object> param;
+}
+
 public class UIBasic : MonoBehaviour
 {
     protected UIPanel panel;
     protected GComponent ui;
+    protected bool isInited = false;
 
     protected virtual void Awake()
     {
@@ -25,12 +33,81 @@ public class UIBasic : MonoBehaviour
     {
         panel = gameObject.GetComponent<UIPanel>();
         ui = panel.ui;
+        if (!isInited)
+        {
+            Debug.LogError($"no inited! {GetType().ToString()}");
+        }
     }
 
     // 初始化
     public virtual void Init(UIParamBasic param = null)
     {
         UIController.Instance.onSetUI(param != null ? param.UIName : GetType().ToString(), this);
+        isInited = true;
+    }
 
+    // 下面多语言处理有个问题，就是List存了10个列表，释放的时间没法控制，可能会有内存泄漏，包括被删掉的ui在这里没法清除 
+
+    // key 为 UI名，即 _alias ，GObject为具体文本 TextParam为参数 其默认值为另一个key，用于在表中查询文本
+    protected Dictionary<string, GObject> dic_Key_UI = new Dictionary<string, GObject>();
+    protected Dictionary<string, TextParam> dic_Key_Param = new Dictionary<string, TextParam>();
+
+    protected virtual void OnSetText(string key, GObject obj, TextParam value = null)
+    {
+        if (!dic_Key_UI.ContainsKey(key))
+        {
+            dic_Key_UI.Add(key, obj);
+        }
+        else
+        {
+            dic_Key_UI[key] = obj;
+        }
+        if (dic_Key_Param.ContainsKey(key))
+        {
+            dic_Key_Param.Add(key, value);
+        }
+        else
+        {
+            dic_Key_Param[key] = value;
+        }
+        SetText(key);
+    }
+
+    protected virtual void SetText(string key)
+    {
+        if (dic_Key_UI.TryGetValue(key, out GObject gObject))
+        {
+            if (gObject.isDisposed)
+            {
+                dic_Key_UI.Remove(key);
+                if (dic_Key_Param.ContainsKey(key))
+                {
+                    dic_Key_Param.Remove(key);
+                }
+                return;
+            }
+            if (dic_Key_Param.TryGetValue(key, out TextParam value))
+            {
+                if (value.param != null)
+                {
+                    gObject.text = Localization.GetFormat(value.defaultValue, value.param.ToArray());
+                }
+                else
+                {
+                    gObject.text = Localization.Get(value.defaultValue);
+                }
+            }
+        }
+    }
+
+    public void OnLocalize()
+    {
+        // 遍历上面的字典，重新设置
+        foreach(var item in dic_Key_UI)
+        {
+            SetText(item.Key);
+        }
+        //var direction = Localization.Instance.SelectedLanguageDirection;
+        //if (text != null && !maintainTextAlignment) UpdateAlignment(text, direction);
     }
 }
